876d8aad30b0ae2c0b12d946b93875b03d35281b
Mikah Mathews <mikah@outofband.org>
2020-04-10 10:10:51 -0700

    Add pickup prototype and actual price calculator (hopefullly)

--- js/scripts.js
+++ js/scripts.js
@@ -6,18 +6,33 @@ function pizza(size, toppings, pickUpMethod) {
 
 pizza.prototype.size = function(s) {
   if(s === "small") {
-    return "small";
+    return 10;
   } else if(s === "medium") {
-    return "medium";
+    return 12;
   } else if(s === "large") {
-    return "large";
+    return 15;
   } else {
     console.log("something went wrong in size");
   }
 }
 
 pizza.prototype.toppings = function(amountChecked) {
-  var toppingsPrice = toppings * .5;
+  var toppingsPrice = amountChecked * .5;
   return toppingsPrice;
+}
+
+pizza.prototype.pickUpMethod = function(selected) {
+  if(selected === "pickUp") {
+    return 0;
+  } else if(selected === "deliver") {
+    return 10;
+  } else {
+    console.log("Something went wrong in pickUpMethod");
+  }
+}
+
+pizza.prototype.finalPrice(size, toppings, pickUpMethod) {
+  var price = size + toppings + pickUpMethod;
+  return price;
 }
 
